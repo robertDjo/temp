700697ef80c9705ba3418250ca7598a90e984a1c
robertDjo <40612253+robertDjo@users.noreply.github.com>
2022-10-10 12:06:25 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="scripts.js"></script>
+    <style>
+        *{
+            margin: 0;
+            padding: 0;
+        }
+    </style>
+</head>
+<body>
+    <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis sunt quam voluptatem, perferendis quisquam, quos eum unde possimus, dolorem quas eveniet cupiditate atque? Id ea officiis optio odit modi adipisci.</h1>
+</body>
+</html>
